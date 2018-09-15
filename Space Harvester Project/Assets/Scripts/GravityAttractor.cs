28b641a35e0a6260459a4235ad64004af6bb82de
Cynthia Lee <35241156+Cynthia-Lee@users.noreply.github.com>
2018-09-14 20:14:59 -0400

    Added scripts for gravity on planet

---
+++ Space Harvester Project/Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAttractor : MonoBehaviour {
+
+    public float gravity = -10f;
+
+    //will be called by each gravity body in the scene
+    public void Attract(Transform body) {
+        Vector3 targetDir = (body.position - transform.position).normalized;
+        // normalized to be always the same length, because it is a direction
+        Vector3 bodyUp = body.up;
+
+        body.rotation = Quaternion.FromToRotation(bodyUp, targetDir) * body.rotation;
+        //supplying the rotation
+
+        //Apply downwards force to the object
+
+        body.GetComponent<Rigidbody>().AddForce(targetDir * gravity);
+        //body.rigidbody.AddForce(targetDir * gravity);
+        
+    } 
+        
+
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
---
+++ Space Harvester Project/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   [RequireComponent (typeof (Rigidbody))]
+public class GravityBody : MonoBehaviour {
+
+    GravityAttractor planet;
+
+    void Awake(){
+        planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        //don't want rigid bodies to do its own rotation
+
+    }
+
+    private void FixedUpdate()
+    {
+        planet.Attract(transform);
+    }
+
+    // Use this for initialization
+    //void Start () {
+
+    //}
+
+    // Update is called once per frame
+    //void Update () {
+
+    //}
+}
