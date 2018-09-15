0367dae17c5ca23ed44f5166b2ba56c46e42c5b9
Cynthia Lee <35241156+Cynthia-Lee@users.noreply.github.com>
2018-09-14 21:55:05 -0400

    Added walking movement

--- Space Harvester Project/Assets/Scripts/FirstPersonController.cs
+++ Space Harvester Project/Assets/Scripts/FirstPersonController.cs
@@ -7,7 +7,7 @@ public class FirstPersonController : MonoBehaviour {
     public float mouseSensitivityX = 250f;
     public float mouseSensitivityY = 250f;
     public float walkSpeed = 8f;
-    public float runSpeed; //GET TO THIS LATER, if the run key is down then multiply targetMoveAmount = moveDir * runspeed
+    //public float runSpeed; //GET TO THIS LATER, if the run key is down then multiply targetMoveAmount = moveDir * runspeed
 
     Transform cameraT;
     float verticalLookRotation; // ~60 to 60
@@ -22,13 +22,14 @@ public class FirstPersonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivityX);
         transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivityX);
         verticalLookRotation += Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivityY;
         //need to clamp this, limit top and bottom
         verticalLookRotation = Mathf.Clamp(verticalLookRotation, -60, 60);
         cameraT.localEulerAngles = Vector3.left * verticalLookRotation;
 
-        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Veritcal")).normalized;
+        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         //a d or left right key
         Vector3 targetMoveAmount = moveDir * walkSpeed;
         moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
